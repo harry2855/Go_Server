3d02fb59c758416fce18c0a25bf6f9ae4f406322
harry2855 <hemanshuraheja@gmail.com>
2024-09-24 01:36:30 +0530

    counter

--- main.go
+++ main.go
@@ -2,11 +2,45 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 )
-func main(){
-	mux := http.NewServeMux()
-	mux.Handle("/",http.FileServer(http.Dir(".")))
-	server := &http.Server{Addr: ":8080",Handler: mux}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+
+func handleHealthz(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) countHandler(w http.ResponseWriter,r *http.Request){
+	w.Header().Add("Content-Type","text/plain; charset=utf-8")
+	hitsStr := strconv.Itoa(cfg.fileserverHits)
+	w.Write([]byte("Hits: "+hitsStr))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// ...
 	
+	return http.HandlerFunc(func(w http.ResponseWriter,r *http.Request){
+		cfg.fileserverHits++
+		next.ServeHTTP(w,r)
+	})
+}
+
+
+
+func main(){
+	cfg := apiConfig{0}
+	mux := http.NewServeMux()
+	mux.Handle("/app/",cfg.middlewareMetricsInc(http.StripPrefix("/app/",http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz",handleHealthz)
+	mux.HandleFunc("/metrics",cfg.countHandler)
+	mux.HandleFunc("/reset",cfg.resetHandler)
+
+	server := &http.Server{Addr: ":8080",Handler: mux}	
 	server.ListenAndServe()
 }
